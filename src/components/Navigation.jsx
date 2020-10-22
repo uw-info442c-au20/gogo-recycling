36cd91208ec8cd728a239b2be236d1ee0a70420e
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-10-22 00:06:45 -0700

    👢 Add in bootstrap

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,12 +4,27 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <nav>
-            Navigation
-            <br/>
-            <Link to="/">Home</Link>
-            <br/>
-            <Link to="/test">Test</Link>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link className="navbar-brand" to="/">
+                Gogo Recycling
+            </Link>
+            <button
+                className="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarNav" aria-controls="navbarNav"
+                aria-expanded="false" aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/test">Test</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
