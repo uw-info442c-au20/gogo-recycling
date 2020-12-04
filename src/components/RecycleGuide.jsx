4be71b554fe8fc4d7f0247fd332844a798040129
anushka <anushka@cs.washington.edu>
2020-12-04 00:59:01 -0800

    Fix behavior when no locations are found in Recycling Guide

--- src/components/RecycleGuide.jsx
+++ src/components/RecycleGuide.jsx
@@ -12,16 +12,26 @@ const RecycleGuide = () => {
     // Set default options
     useEffect(() => {
         setOptions({
-            zip: "98275"
+            zip: "",
+            city: ""
         });
     }, []);
 
-    const updateZip = event => {
-        let zip = event.target.value;
-        if (zip.length === 5) {
+    const updateZipOrCity = event => {
+        let input = event.target.value;
+        if (input.length === 5 && !isNaN(input)) {
             setOptions(options => {
                 let newOptions = { ...options };
-                newOptions.zip = zip;
+                newOptions.zip = input;
+                newOptions.city = "";
+                return newOptions;
+            });
+        } else {
+             setOptions(options => {
+                console.log("city", options);
+                let newOptions = { ...options };
+                newOptions.city = input;
+                newOptions.zip = "";
                 return newOptions;
             });
         }
@@ -33,12 +43,14 @@ const RecycleGuide = () => {
                 <h1 className="p-4">Recycling Guide</h1>
                 <div className="search p-3 px-4">
                     <span className="pr-3">I want to recycle at</span>
-                    <input type="text" className="input-item" placeholder="Zipcode" defaultValue={options.zip} onChange={updateZip} />
+                    <input type="text" className="input-item" placeholder="Zipcode or City" 
+                    onChange={updateZipOrCity} />
                     <img className="pl-2 mb-1" src="https://www.flaticon.com/svg/static/icons/svg/93/93642.svg" alt="tilted magnifying glass"/>
                 </div>
                 <div className="results pb-5">
                     <h1 className="pt-5 pb-3">Recycling Locations</h1>
-                    {locations.length > 0 ? locations.map((location, index) => {
+                    {locations.length > 0 && (options.zip != "" || options.city != "") ?
+                     locations.map((location, index) => {
                         const {
                             provider_name, provider_address, provider_url, phone, providerid
                         } = location;
@@ -61,10 +73,10 @@ const RecycleGuide = () => {
                                 </div>
                             </div>
                         );
-                    }) : <p>No Locations Found</p>}
+                    }) : <p>No Locations Found</p>
+                    }
                 </div>
             </div>
-
             <Copyright />
         </main>
     );
