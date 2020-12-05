9b29a7ece5d6d393bcc5c7514b2ddc38dc015aa8
Sruthi Dikkala <dikkala@uw.edu>
2020-12-04 17:03:08 -0800

    editted homepage

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -2,32 +2,57 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 import recyclePromo from '../resources/home-banner/recycle-promo.jpg';
-import cycleEconomy from "../resources/home-banner/cycle-economy.png";
+import sustainableLiving from "../resources/home-banner/sustainable-living.png";
 
 export default function CenteredGrid() {
     return (
-        <React.Fragment> 
+        <React.Fragment>
             <div id="stat-1" className="shadow my-5">
-                {/* inspiring quote emphasizing buyers impact the movement of business */}
+                {/* inspiring quote about the importance of individual contributions to sustainable living */}
                 <div className="p-3">
-                    <h1>“Recycling, packaging, businesses are changing all of those things because that’s what consumers want.”</h1>
-                    <h2>&mdash;Jerry Greenfield, Co-founder of Ben &amp; Jerry’s Ice Cream&mdash;</h2>
+                    <h1>“Each one of us has the moral responsibility to do the absolute best that we can.”</h1>
+                    <h2>&mdash;Christiana Figueres, Executive Secretary, UNFCCC&mdash;</h2>
                 </div>
             </div>
+
+            <div id="stat-1" className="shadow my-5">
+                {/* video on the connection between COVID and sustainable living */}
+                <div className="p-3">
+                <h2>Sustainable Living During COVID</h2>
+                    <iframe width="500" height="320" src="https://www.youtube.com/embed/R1h1L6vj3BI"></iframe>
+                </div>
+            </div>
+
             <div id="info-container">
                 <div id="stat-2" className="shadow p-4">
-                    <h2>First Step: Proper Recycling</h2>
+                    <h2>Proper Recycling</h2>
                     <img id="stat2" src={recyclePromo} alt="People advertising proper recycling" />
-                    <p className="px-3">INTRODUCE BUSINESS THAT PRACTICE CIRCULAR ECONOMY. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature. from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,.looked up one of the more obscure Latin words, consectetur, e of classical Latin literature. from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,.looked up one of the more obscure Latin words, consectetur.</p>
-                    <Link to="/"><button className="btn btn-main">Easy Recycling Guide</button></Link>
+                    <p className="px-3">
+                        <ul>
+                        <li>Make recycling a collaborative effort where everyone in the family participates, enabling the most recycling of the right materials.</li>
+                        <li>Recycling arrows on a container don’t necessarily mean it’s recyclable. Check with local program guidelines.</li>
+                        <li>Make sure containers are clean enough when recycling them to avoid contaminating other materials.</li>
+                        <li>Not all types of glass bottles and jars are recyclable. Make sure to check your jurisdiction and community’s recycling guidelines.</li>
+                        <li>Don’t recycle items if you’re not sure, they won’t get sorted and will contaminate recyclable materials.</li>
+                        </ul>
+                    </p>
+                    {/* <Link to="/"><button className="btn btn-main">Easy Recycling Guide</button></Link> */}
                 </div>
                 <div id="stat-3" className="shadow p-4">
                     <h2>Beyond Recycling</h2>
-                    <img id="stat3" src={cycleEconomy} alt="Circular Economy" />
-                    <p className="px-3">INTRODUCE BUSINESS THAT PRACTICE CIRCULAR ECONOMY. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature. from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,.looked up one of the more obscure Latin words, consectetur, e of classical Latin literature. from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,.looked up one of the more obscure Latin words, consectetur.</p>
-                    <Link to="/"><button className="btn btn-main">Explore Business with Circular Economy</button></Link>
+                    <img id="stat3" src={sustainableLiving} alt="Sustainable Living" />
+                    <p className="px-3">
+                        <ul>
+                        <li>Get educated and stay updated on the latest news to make more informed decisions.</li>
+                        <li>Limit waste by using reusable, eco-friendly products and reducing packaging.</li>
+                        <li>Conserve water and energy at home during daily use.</li>
+                        <li>Advocate for change and promote sustainable living in your personal and professional networks.</li>
+                        <li>Use more eco-friendly methods of transportation such as walking, biking, public transportation or carpooling.</li>
+                        </ul>
+                    </p>
+                    {/* <Link to="/"><button className="btn btn-main">Explore Business with Circular Economy</button></Link> */}
                 </div>
             </div>
-        </React.Fragment> 
+        </React.Fragment>
     );
 }
