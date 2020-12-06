571638a591ee331432fcaf067bc455971ed56517
Kayla Chea <kaylamchea@hotmail.com>
2020-12-06 12:39:51 -0800

    :sparkles: Add profile page feature

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+
 import { useLocation } from "react-router-dom";
 import usePosts from "../hooks/usePosts";
 
 import useUser from "../hooks/useUser";
 import anon from "../resources/anon.png";
-import manRecycling from "../resources/man-recycling.jpg";
 
 const Profile = () => {
     const userID = useLocation().pathname.split("/")[2];
@@ -18,43 +18,26 @@ const Profile = () => {
                     <img
                         className="pb-1 rounded-circle" alt="Anonymous user icon"
                         src={anon}
-                    />
-                    {/* Potential Badge */}
-                    <p>Botanical Extraordinaire</p>
+                    /> 
                 </div>
                 <div className="profile-text pl-5">
                     <div className="profile-text2">
                         <h1 className="mb-1">{user.displayName}</h1>
-                        {/* Follow button if possible*/}
-                        <button className="btn ml-3">
-                            <img
-                                src="https://www.flaticon.com/svg/static/icons/svg/983/983886.svg"
-                                alt="person behind a plus sign" aria-label="Follow"
-                            />
-                        </button>
                     </div>
-                    <p className="mb-1">Hi this is an example short bio</p>
                     <p className="mb-0">
                         <span className="post-amount">{user.points}</span> points
                         | <span className="post-amount">{posts.length}</span> posts
                     </p>
                 </div>
             </div>
-            {/* Use modal to display extra info */}
-            <div className="profile-posts pt-4">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm">
-                            <img src={manRecycling} alt="man recycling"/>
-                        </div>
-                        <div className="col-sm">
-                            <img src={manRecycling} alt="man recycling"/>
-                        </div>
-                        <div className="col-sm">
-                            <img src={manRecycling} alt="man recycling"/>
-                        </div>
+            <div className="profile-posts">
+                {posts.length > 0 && posts.map(post =>
+                    <div className="m-3 shadow">
+                        <img className="pb-2" src={post.images}/>
+                        <p className="px-2 m-0">{post.description}</p>
+                        <p className="m-2">{post.likes.length} like(s)</p>
                     </div>
-                </div>
+                )}
             </div>
         </main>
     : <main><h1>Loading...</h1></main>);
